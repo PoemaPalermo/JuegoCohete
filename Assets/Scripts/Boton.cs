033754bf718828f148e24ea704dfadbb1c3eb9ae
PoemaPalermo <163904623+PoemaPalermo@users.noreply.github.com>
2024-04-04 16:32:37 -0300

    Initial commit

---
+++ Assets/Scripts/Boton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour
+{
+    public GameObject botonNormalGO;
+    public GameObject botonPresionadoGO;
+    public CoheteScript coheteScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnMouseUp()
+    {
+        botonNormalGO.SetActive(true);
+        botonPresionadoGO.SetActive(false);
+        coheteScript.speed = 0;
+    }
+    private void OnMouseDown()
+    {
+        botonNormalGO.SetActive(false);
+        botonPresionadoGO.SetActive(true);
+        coheteScript.speed = 0.5f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ControlesTeclado.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlesTeclado : MonoBehaviour
+{
+    public float speed;
+    public float movX;
+    public float maxX= 11.5f;
+    public float minX= -11.5f;
+    public float movY;
+    public float maxY = 2.3f;
+    public float minY = -4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movX = Input.GetAxis("Horizontal");
+        transform.Translate(movX*speed * Time.deltaTime,0,0);
+
+        if(transform.position.x>maxX)
+        {
+            transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < minX)
+        {
+            transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+        }
+
+        movY = Input.GetAxis("Vertical");
+        transform.Translate(0, movY * speed * Time.deltaTime, 0);
+
+        if (transform.position.y > maxY)
+        {
+            transform.position = new Vector3(transform.position.x, maxY , transform.position.z);
+        }
+        else if (transform.position.y < minY)
+        {
+            transform.position = new Vector3(transform.position.x, minY , transform.position.z);
+        }
+    }
+}
